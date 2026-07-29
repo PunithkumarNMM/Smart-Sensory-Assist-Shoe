c6c3a6a066cfff876b7b722e3e328bc4a6a639ad
PunithkumarNMM <punithkumar7117@gmail.com>
2026-07-29 16:55:14 +0530

    Add profile image support

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -47,13 +47,14 @@ const login = async (req, res) => {
             success: true,
             message: "Login Successful",
             token,
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email,
-                phone: user.phone,
-                role: user.role
-            }
+           user: {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    phone: user.phone,
+    role: user.role,
+    profileImage: user.profileImage
+}
         });
 
     } catch (error) {
