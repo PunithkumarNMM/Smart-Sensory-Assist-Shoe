339cb08e960e3e8db0367f38f6902500e1a394b1
PunithkumarNMM <punithkumar7117@gmail.com>
2026-07-29 17:31:13 +0530

    Complete dashboard API integration and profile upload updates

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -3,14 +3,16 @@ const Location = require("../models/Location");
 const FallEvent = require("../models/FallEvent");
 const SOSEvent = require("../models/SOSEvent");
 const Notification = require("../models/Notification");
-
+const User = require("../models/User");
 const getDashboard = async (req, res) => {
   try {
     const { deviceId } = req.params;
 
     // Device Details
     const device = await Device.findOne({ deviceId });
-
+const user = device
+  ? await User.findById(device.parentId)
+  : null;
     // Latest GPS
     const latestLocation = await Location.findOne({ deviceId })
       .sort({ createdAt: -1 });
@@ -29,15 +31,19 @@ const getDashboard = async (req, res) => {
       .limit(10);
 
     res.status(200).json({
-      success: true,
-      data: {
-        device,
-        latestLocation,
-        latestFall,
-        latestSOS,
-        notifications,
-      },
-    });
+  success: true,
+  data: {
+    name: device?.ownerName || "",
+    email: device?.ownerEmail || "",
+    profileImage: device?.profileImage || "",
+    deviceStatus: device ? "Connected" : "Disconnected",
+    battery: device?.battery || 0,
+    lastSync: device?.updatedAt || "",
+    location: latestLocation
+      ? `${latestLocation.latitude}, ${latestLocation.longitude}`
+      : "Unknown"
+  }
+});
   } catch (error) {
     res.status(500).json({
       success: false,
